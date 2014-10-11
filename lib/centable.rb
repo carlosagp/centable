5a103149ffac5c9721437ec041a70498aab25285
Carlos Garcia <carlos@par9golf.com>
2014-10-11 15:20:18 -0600

    Add main logic to Centable

--- lib/centable.rb
+++ lib/centable.rb
@@ -1,4 +1,23 @@
 require "centable/engine"
 
 module Centable
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def cent_column(attribute, options={})
+      cents_col = "#{attribute.to_s}_cents".to_sym
+      define_method("#{attribute.to_s}=") do |val|
+        num_val = cast_to_numeric(val)
+        self[cents_col] = num_val.nil? ? 0 : (num_val*100).to_i
+      end
+
+      define_method("#{attribute.to_s}") do
+        self[cents_col] ? BigDecimal(self[cents_col])/100 : self[cents_col]
+      end
+    end
+  end
+
+  def cast_to_numeric(value)
+    value.is_a?(String) ? value.to_big_decimal : value
+  end
 end
